797a39ba1c31b9fe0d4d77e0e18a688077c3f10d
Eric Sanchez <ericfsod@gmail.com>
2021-07-05 13:38:09 -0400

    commit tree 9a4ee1d3c1ee97ce1f68805ddf1a509b3e228ec9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+//You don't need to wrap the log in a variable returning the filtered array/object is enough as the condition and return is housed in the anonymous function.
+
+function findMatching (drivers, name){
+   return drivers.filter(currantNameInArray => currantNameInArray.toLowerCase() === name.toLowerCase())
+        
+            //return newArray;
+        }
+
+    // findMatching('Bobby') 
+
+    function fuzzyMatch (drivers, str){
+
+            return drivers.filter(currantNameInArray => currantNameInArray[0] === str[0])
+            //return fuzzyArray
+        }
+   
+        function matchName (drivers, name){
+            return drivers.filter(driver => driver.name === name)
+
+            //filter is returning an array with the requested logic. No need for conditionals or to wrap the logic in it's own array
+            
+
+        }
